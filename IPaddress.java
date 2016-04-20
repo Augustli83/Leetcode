699e827053165e644171d0edff07eb1dd2b047a1
Augustli83 <auguli@hotmail.com>
2016-04-20 22:18:32 +0800

    IPadd

---
+++ IPaddress.java
@@ -0,0 +1,34 @@
+public class Solution {
+    List<String> ipAdd = new ArrayList<>();
+    List<String> list = new ArrayList<>();
+	public List<String> restoreIpAddresses(String s) {
+        restoreIP(s,0);
+        return ipAdd;
+    }
+	public void restoreIP(String s, int pos){
+		if(pos==s.length() && list.size()==4){
+			ipAdd.add(list.get(0) + "." + list.get(1) + "." + list.get(2) +"." + list.get(3));
+			return;
+		}
+		if(list.size()>4){
+			return;
+		}
+		if(s.length()-pos<=0){
+			return;
+		}
+		for(int i=1;i<4;i++){
+			if(s.length()-pos>=i){
+				String sub=s.substring(pos,pos+i);
+				int subNum=Integer.parseInt(sub);
+				if(sub.length()>1 && sub.startsWith("0")){
+					continue;
+				}
+				if(subNum >= 0 && subNum <=255){
+					list.add(sub);
+					restoreIP(s,pos+i);
+					list.remove(list.size()-1);
+				}
+			}
+		}
+	}
+}
